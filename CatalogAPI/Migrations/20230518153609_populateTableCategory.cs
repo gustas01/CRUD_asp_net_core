bd69e1595049467846d951b2171c04141fe89917
gustas01 <gustasboyevolution@hotmail.com>
2023-05-18 13:38:32 -0300

    criando migrations para popular as tebelas (seeds)

---
+++ CatalogAPI/Migrations/20230518153609_populateTableCategory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class populateTableCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into \"Categories\"(\"Name\", \"ImageURL\") Values('Bebidas', 'bebidas.jpg')");
+            mb.Sql("Insert into \"Categories\"(\"Name\", \"ImageURL\") Values('Lanches', 'lanches.jpg')");
+            mb.Sql("Insert into \"Categories\"(\"Name\", \"ImageURL\") Values('Sobremesa', 'sobremesa.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from \"Categories\"");
+        }
+    }
+}
---
+++ CatalogAPI/Migrations/20230518162249_populateTableProduct.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class populateTableProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into \"Products\"(\"Name\", \"Description\", \"Price\", \"ImageURL\", \"storage\", \"RegisterDate\", \"CategoryId\") Values('Coca-Cola Diet', 'Refrigerante de cola 350 ml', 5.45, 'cocacola.jpg', 50, now(), 6)");
+            
+            mb.Sql("Insert into \"Products\"(\"Name\", \"Description\", \"Price\", \"ImageURL\", \"storage\", \"RegisterDate\", \"CategoryId\") Values('x-tudão', 'lanche grande', 10.00, 'xtudao.jpg', 50, now(), 7)");
+            
+            mb.Sql("Insert into \"Products\"(\"Name\", \"Description\", \"Price\", \"ImageURL\", \"storage\", \"RegisterDate\", \"CategoryId\") Values('Pudim', 'Pudim de chocolate 150 ml', 6.69, 'pudim.jpg', 50, now(), 8)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("delete from \"Products\"");
+        }
+    }
+}
