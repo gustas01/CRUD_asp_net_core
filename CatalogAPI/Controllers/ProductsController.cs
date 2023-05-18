45d266e5c88410eadab17165649562226114d88e
gustas01 <gustasboyevolution@hotmail.com>
2023-05-18 15:38:51 -0300

    implementando o delete de Product

--- CatalogAPI/Controllers/ProductsController.cs
+++ CatalogAPI/Controllers/ProductsController.cs
@@ -47,4 +47,15 @@ public class ProductsController : ControllerBase
 
     return Ok(product);
   }
+
+  [HttpDelete("{id:int}")]
+  public ActionResult Delete(int id){
+    Product product = _context.Products.FirstOrDefault(p => p.ProductId == id);
+    if(product is null) return NotFound("Produto não encontrado");
+
+    _context.Products.Remove(product);
+    _context.SaveChanges();
+
+    return Ok(product);
+  }
 }
